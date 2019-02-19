7668f90ad6b57597ef8b66ebb79f3e8eb3b2909e
Navin Kumar <navinkumar2508@gmail.com>
2019-02-19 21:59:50 +0530

    TECH:Navin | generated model and added seed script | dummy data

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_19_144119) do
+ActiveRecord::Schema.define(version: 2019_02_19_161416) do
+
+  create_table "graphs", force: :cascade do |t|
+    t.string "name"
+    t.float "PI"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
